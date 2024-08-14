afbb4bc75de3598772250eaf84f130db7ae98a6c
ibaragozz <baragozz1@gmail.com>
2024-08-14 18:18:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,37 @@ class Warrior():
         self.hair_color = hair_color
 
     def eat(self):
-        print(f” {self.name} сел кушать”)
+        print(f'{self.name} сел кушать')
         self.power += 1
 
     def sleep(self):
-        print(f” {self.name} лег поспать”)
+        print(f'{self.name} лег поспать')
         self.endurance += 2
 
     def fight(self):
-        print(f” {self.name} сражается”)
+        print(f'{self.name} сражается”')
         self.endurance -= 6
 
     def walk(self):
-        print(f” {self.name} гуляет”)
+        print(f'{self.name} гуляет')
 
     def info(self):
-        print(f”имя воина - {self.name}”)
-        print(f”цвет волос воина - {self.hair_color}”)
-        print(f”сила воина - {self.power}”)
-        print(f”выносливость воина - {self.endurance}”)
+        print(f'имя воина - {self.name}')
+        print(f'цвет волос воина - {self.hair_color}')
+        print(f'сила воина - {self.power}')
+        print(f'выносливость воина - {self.endurance}')
+
+war1 = Warrior('Леголас', 25, 55, 'Блондин')
+war2 = Warrior('Боромир', 33, 45, 'Брюнет')
+
+print(war2.endurance)
+print(war2.power)
+
+war2.eat()
+war2.sleep()
+
+print(war2.endurance)
+print(war2.power)
+
+war1.info()
+war2.info()
